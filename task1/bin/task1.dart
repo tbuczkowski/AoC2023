5c1234caf1930f8d32d02cf323dee505ddef656a
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-01 10:19:45 +0100

    task 1 part 2

--- task1/bin/task1.dart
+++ task1/bin/task1.dart
@@ -1,4 +1,6 @@
-final RegExp numbers = RegExp(r'[0-9]');
+import 'dart:io';
+
+final RegExp numbers = RegExp(r'(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))');
 
 int _computeLine(String line) {
   String? firstChar, lastChar;
@@ -19,8 +21,36 @@ int _computeLine(String line) {
   return int.parse(firstChar! + lastChar!);
 }
 
+int _mapToDigit(String match) => switch (match) {
+      '1' || 'one' => 1,
+      '2' || 'two' => 2,
+      '3' || 'three' => 3,
+      '4' || 'four' => 4,
+      '5' || 'five' => 5,
+      '6' || 'six' => 6,
+      '7' || 'seven' => 7,
+      '8' || 'eight' => 8,
+      '9' || 'nine' => 9,
+      _ => throw Exception('Invalid number'),
+    };
+
+int _computeLine2(String line) {
+  final matches = numbers.allMatches(line).toList();
+  final int firstDigit = _mapToDigit(matches.first.group(1)!);
+  final int lastDigit = _mapToDigit(matches.last.group(1)!);
+  final int sum = firstDigit * 10 + lastDigit;
+  print(sum);
+  return sum;
+}
+
 Future<void> main(List<String> arguments) async {
-  // final List<int> results = await Future.wait(arguments.map((String line) => compute(_computeLine, line)).toList());
-  final List<int> results = arguments.map((e) => _computeLine(e)).toList();
+  final List<String> input = loadInputData(arguments.first);
+  final List<int> results = input.map((e) => _computeLine2(e)).toList();
   print(results.fold<int>(0, (int sum, int element) => sum + element));
+}
+
+List<String> loadInputData(String filename) {
+  final File file = File(filename);
+  final List<String> lines = file.readAsLinesSync();
+  return lines;
 }
