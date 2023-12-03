fd29072afb4b94eb893e53dae5fb844c10d2fd16
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-03 10:20:34 +0100

    task 3 part 2

--- task3/bin/task3.dart
+++ task3/bin/task3.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 late final List<List<String>> _grid;
 final List<Sequence> _sequences = [];
+final List<(int, int)> _gears = [];
 
 class Sequence {
   final (int, int) startCoords;
@@ -52,28 +53,24 @@ class Sequence {
 void main(List<String> arguments) {
   final List<String> input = loadInputData(arguments.first);
   _grid = input.map((String line) => line.split('').toList(growable: false)).toList(growable: false);
-  int globalSum = 0;
   for (int y = 0; y < _grid.length; y++) {
     final List<String> row = _grid[y];
-    int rowSum = 0;
-    String rowSumStr = '';
     int? sequenceStart;
     for (int x = 0; x < row.length; x++) {
       final String symbol = row[x];
       if (x == row.length - 1 && ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].contains(symbol)) {
         if (sequenceStart != null) {
           _sequences.add(Sequence((y, sequenceStart!), x - sequenceStart! + 1));
-          rowSum += _sequences.last.isPartNumber ? _sequences.last.value : 0;
-          rowSumStr += '+${_sequences.last.isPartNumber ? _sequences.last.value : 0}';
           sequenceStart = null;
         }
+      }
+      if (x == row.length - 1 && symbol == '*') {
+        _gears.add((y, x));
       }
       (switch (symbol) {
         '.' => () {
             if (sequenceStart != null) {
               _sequences.add(Sequence((y, sequenceStart!), x - sequenceStart!));
-              rowSum += _sequences.last.isPartNumber ? _sequences.last.value : 0;
-              rowSumStr += '+${_sequences.last.isPartNumber ? _sequences.last.value : 0}';
               sequenceStart = null;
             }
           },
@@ -83,19 +80,60 @@ void main(List<String> arguments) {
         _ => () {
             if (sequenceStart != null) {
               _sequences.add(Sequence((y, sequenceStart!), x - sequenceStart!));
-              rowSum += _sequences.last.isPartNumber ? _sequences.last.value : 0;
               sequenceStart = null;
+            }
+            if (symbol == '*') {
+              _gears.add((y, x));
             }
           },
       })();
     }
-    globalSum += rowSum;
-    print(rowSumStr + '=' + rowSum.toString());
   }
   _sequences.removeWhere((Sequence sequence) => !sequence.isPartNumber);
-  // print(_sequences.fold<String>('', (String previousValue, element) => previousValue + element.toString() + '\n'));
-  final int result = _sequences.fold(0, (int sum, Sequence sequence) => sum + sequence.value);
-  print(result);
+  // final int result = _sequences.fold(0, (int sum, Sequence sequence) => sum + sequence.value);
+  int gearRatiosSum = 0;
+  for ((int, int) gearCoords in _gears) {
+    gearRatiosSum += _findGearRatio(gearCoords);
+  }
+  print(gearRatiosSum);
+}
+
+Sequence? _findSequenceForCoords((int, int) coords) {
+  try {
+    return _sequences.firstWhere(
+      (Sequence sequence) =>
+          coords.$1 == sequence.startCoords.$1 &&
+          coords.$2 >= sequence.startCoords.$2 &&
+          coords.$2 < sequence.startCoords.$2 + sequence.length,
+    );
+  } catch (e) {
+    return null;
+  }
+}
+
+int _findGearRatio((int, int) gearCoords) {
+  final Set<Sequence> adjacentSequences = {};
+  for (int y = -1; y <= 1; y++) {
+    for (int x = -1; x <= 1; x++) {
+      final (int, int) neighborCoords = (gearCoords.$1 + x, gearCoords.$2 + y);
+      if (neighborCoords.$1 >= _grid.length ||
+          neighborCoords.$1 < 0 ||
+          neighborCoords.$2 >= _grid[0].length ||
+          neighborCoords.$2 < 0) {
+        continue;
+      }
+      final Sequence? adjacentSequence = _findSequenceForCoords(neighborCoords);
+      if (adjacentSequence != null) {
+        adjacentSequences.add(adjacentSequence);
+      }
+    }
+  }
+  if (adjacentSequences.length == 1) {
+    return 0;
+  }
+  return adjacentSequences.fold(1, (int result, Sequence sequence) {
+    return result * sequence.value;
+  });
 }
 
 List<String> loadInputData(String filename) {
