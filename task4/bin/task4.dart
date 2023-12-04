afebd988b9d8ffd00f7f4e4512b3bc7bdc09b4f4
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-04 08:26:01 +0100

    task 4 part 2

--- task4/bin/task4.dart
+++ task4/bin/task4.dart
@@ -1,28 +1,39 @@
 import 'dart:io';
-import 'dart:math';
 
 class Card {
   final int id;
   final Set<int> winningNumbers;
   final Set<int> cardNumbers;
+  late final int numberOfMatches;
 
   Card({
     required this.id,
     required this.winningNumbers,
     required this.cardNumbers,
-  });
+  }) {
+    final Set<int> matches = winningNumbers.intersection(cardNumbers);
+    numberOfMatches = matches.length;
+  }
+
+  @override
+  String toString() {
+    return 'Card{id: $id, numberOfMatches: $numberOfMatches}';
+  }
 }
 
 void main(List<String> arguments) {
   final List<String> lines = loadInputData(arguments.first);
   final List<Card> cards = parseCards(lines);
-  int sumOfPoints = 0;
+  final Map<Card, int> cardCount = {for (Card card in cards) card: 1};
   for (Card card in cards) {
-    final Set<int> matches = card.winningNumbers.intersection(card.cardNumbers);
-    final int numberOfMatches = matches.length;
-    sumOfPoints += pow(2, numberOfMatches - 1).toInt();
+    final int numberOfCopies = cardCount[card]!;
+    for (int i = 0; i < card.numberOfMatches; i++) {
+      final Card cardToCopy = cards[card.id + i];
+      final int currentCount = cardCount[cardToCopy]!;
+      cardCount[cardToCopy] = currentCount + numberOfCopies;
+    }
   }
-  print(sumOfPoints);
+  print(cardCount.values.fold<int>(0, (sum, element) => sum + element));
 }
 
 List<Card> parseCards(List<String> lines) {
