813a04964c5a54d9c9577d43a4d76b26f37b4bd8
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-09 09:23:03 +0100

    task 9 part 1

---
+++ task9/bin/task9.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+int processSequence(List<int> sequence) {
+  final List<List<int>> differentials = [];
+  List<int> last = [...sequence];
+  while (differentials.isEmpty || differentials.last.any((element) => element != 0)) {
+    differentials.add([]);
+    for (int i = 1; i < last.length; i++) {
+      differentials.last.add(last[i] - last[i - 1]);
+    }
+    last = [...differentials.last];
+  }
+  for (int i = differentials.length - 1; i >= 0; i--) {
+    if (i == differentials.length - 1) {
+      differentials[i].add(0);
+    } else {
+      differentials[i].add(differentials[i].last + differentials[i + 1].last);
+    }
+  }
+  // print(sequence);
+  // print(differentials);
+  // print(sequence.last + differentials.first.last);
+  return sequence.last + differentials.first.last;
+}
+
+void main(List<String> arguments) {
+  final List<String> lines = loadInputData(arguments.first);
+  int sum = 0;
+  for (String line in lines) {
+    final List<int> numbers = line.split(' ').map((String number) => int.parse(number)).toList();
+    sum += processSequence(numbers);
+  }
+  print(sum);
+}
+
+List<String> loadInputData(String filename) {
+  final File file = File(filename);
+  final List<String> lines = file.readAsLinesSync();
+  return lines;
+}
