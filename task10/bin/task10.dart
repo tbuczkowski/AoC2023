b95601fa4706118eaf88b49aa54177fd761d8095
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-10 20:45:56 +0100

    task 10 part 2

--- task10/bin/task10.dart
+++ task10/bin/task10.dart
@@ -74,18 +74,65 @@ void main(List<String> arguments) async {
   }
   final List<Future<Map<(int, int), int>>> futures = [];
   futures.add(compute(computeDistanceMap, (grid, (startY, startX), (startDirections.first))));
-  futures.add(compute(computeDistanceMap, (grid, (startY, startX), (startDirections.last))));
-  final List<Map<(int, int), int>> results = await Future.wait(futures);
-  int maxDistance = 0;
-  for ((int, int) step in results.first.keys) {
-    final int value1 = results.first[step]!;
-    final int value2 = results.last[step]!;
-    final int m = min(value1, value2);
-    if (m > maxDistance) {
-      maxDistance = m;
+  // futures.add(compute(computeDistanceMap, (grid, (startY, startX), (startDirections.last))));
+  final Map<(int, int), int> results = await futures.first;
+  final List<(int, int)> loop = results.keys.toList();
+  loop.add((startY, startX));
+  grid[startY][startX] = '-';
+  final List<List<String>> grid2 = [
+    for (int i = 0; i < grid.length * 2; i++) [for (int j = 0; j < grid.first.length * 2; j++) 'E']
+  ];
+  for (int y = 0; y < grid.length; y++) {
+    for (int x = 0; x < grid.first.length; x++) {
+      grid2[y * 2][x * 2] = grid[y][x];
     }
   }
-  print(maxDistance);
+  for ((int, int) coord in loop) {
+    final String symbol = grid[coord.$1][coord.$2];
+    grid2[coord.$1 * 2][coord.$2 * 2] = 'X';
+    if (symbol == '|') {
+      grid2[coord.$1 * 2 + 1][coord.$2 * 2] = 'X';
+    } else if (symbol == '-') {
+      grid2[coord.$1 * 2][coord.$2 * 2 + 1] = 'X';
+    } else if (symbol == 'J') {
+      // grid2[coord.$1 * 2 + 1][coord.$2 * 2] = 'X';
+      // grid2[coord.$1 * 2 + 1][coord.$2 * 2 - 1] = 'X';
+    } else if (symbol == 'F') {
+      grid2[coord.$1 * 2][coord.$2 * 2 + 1] = 'X';
+      grid2[coord.$1 * 2 + 1][coord.$2 * 2] = 'X';
+    } else if (symbol == '7') {
+      grid2[coord.$1 * 2 + 1][coord.$2 * 2] = 'X';
+    } else if (symbol == 'L') {
+      grid2[coord.$1 * 2][coord.$2 * 2 + 1] = 'X';
+    }
+  }
+
+  final List<(int, int)> steps = [(0, 0)];
+  while (steps.isNotEmpty) {
+    final (int, int) step = steps.removeLast();
+    grid2[step.$1][step.$2] = 'X';
+    final String top = grid2[max(step.$1 - 1, 0)][step.$2];
+    if (top != 'X' && step.$1 > 0) {
+      steps.add((step.$1 - 1, step.$2));
+    }
+    final String bottom = grid2[min(step.$1 + 1, grid2.length - 1)][step.$2];
+    if (bottom != 'X' && step.$1 < grid2.length - 1) {
+      steps.add((step.$1 + 1, step.$2));
+    }
+    final String left = grid2[step.$1][max(step.$2 - 1, 0)];
+    if (left != 'X' && step.$2 > 0) {
+      steps.add((step.$1, step.$2 - 1));
+    }
+    final String right = grid2[step.$1][min(step.$2 + 1, grid2.first.length - 1)];
+    if (right != 'X' && step.$2 < grid2.first.length - 1) {
+      steps.add((step.$1, step.$2 + 1));
+    }
+    // print(steps);
+  }
+  // for (final l in grid2) {
+  //   print(l.reduce((value, element) => value + element));
+  // }
+  print(grid2.expand((element) => element).where((element) => element != 'E' && element != 'X').length);
 }
 
 List<String> loadInputData(String filename) {
