fb26ca78ab6705a1b84ac0f554799afbbe0bd920
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-14 13:53:14 +0100

    task 14 part 2

--- task14/bin/task14.dart
+++ task14/bin/task14.dart
@@ -1,13 +1,8 @@
 import 'dart:io';
 
-void main(List<String> arguments) async {
-  final List<String> lines = loadInputData(arguments.first);
+import 'package:collection/collection.dart';
 
-  List<List<String>> grid = [];
-  int sum = 0;
-  for (String line in lines) {
-    grid.add(line.split(''));
-  }
+List<List<String>> tiltNorth(List<List<String>> grid) {
   for (int y = 0; y < grid.length; y++) {
     for (int x = 0; x < grid.first.length; x++) {
       final String char = grid[y][x];
@@ -24,9 +19,112 @@ void main(List<String> arguments) async {
       }
     }
   }
+  return grid;
+}
+
+List<List<String>> tiltWest(List<List<String>> grid) {
+  for (int y = 0; y < grid.length; y++) {
+    for (int x = 0; x < grid.first.length; x++) {
+      final String char = grid[y][x];
+      if (char != 'O') {
+        continue;
+      }
+      for (int i = 0; i < x; i++) {
+        final int index = x - i - 1;
+        if (grid[y][index] != '.') {
+          break;
+        }
+        grid[y][index + 1] = '.';
+        grid[y][index] = 'O';
+      }
+    }
+  }
+  return grid;
+}
+
+List<List<String>> tiltSouth(List<List<String>> grid) {
+  for (int y = grid.length - 1; y >= 0; y--) {
+    for (int x = 0; x < grid.first.length; x++) {
+      final String char = grid[y][x];
+      if (char != 'O') {
+        continue;
+      }
+      for (int i = 0; i < grid.length; i++) {
+        final int index = y + i + 1;
+        if (index >= grid.length || grid[index][x] != '.') {
+          break;
+        }
+        grid[index - 1][x] = '.';
+        grid[index][x] = 'O';
+      }
+    }
+  }
+  return grid;
+}
+
+List<List<String>> tiltEast(List<List<String>> grid) {
+  for (int y = 0; y < grid.length; y++) {
+    for (int x = grid.first.length - 1; x >= 0; x--) {
+      final String char = grid[y][x];
+      if (char != 'O') {
+        continue;
+      }
+      for (int i = 0; i < grid.first.length; i++) {
+        final int index = x + i + 1;
+        if (index >= grid.first.length || grid[y][index] != '.') {
+          break;
+        }
+        grid[y][index - 1] = '.';
+        grid[y][index] = 'O';
+      }
+    }
+  }
+  return grid;
+}
+
+void main(List<String> arguments) async {
+  final List<String> lines = loadInputData(arguments.first);
+
+  List<List<String>> grid = [];
+  List<List<String>> originalGrid = [];
+  int sum = 0;
+  for (String line in lines) {
+    grid.add(line.split(''));
+    originalGrid.add(line.split(''));
+  }
+  final DeepCollectionEquality collectionEquality = DeepCollectionEquality();
+  originalGrid = tiltEast(originalGrid);
+  for (int i = 0; i < 1000; i++) {
+    if (i % 5 == 0) {
+      print(i);
+    }
+    grid = tiltNorth(grid);
+    if (collectionEquality.equals(grid, originalGrid)) {
+      print('cycleFound');
+      print(i);
+    }
+    grid = tiltWest(grid);
+    if (collectionEquality.equals(grid, originalGrid)) {
+      print('cycleFound');
+      print(i);
+    }
+    grid = tiltSouth(grid);
+    if (collectionEquality.equals(grid, originalGrid)) {
+      print('cycleFound');
+      print(i);
+    }
+    grid = tiltEast(grid);
+    if (collectionEquality.equals(grid, originalGrid)) {
+      print('cycleFound');
+      print(i);
+    }
+  }
   for (int i = 0; i < grid.length; i++) {
     final int multiplier = grid.length - i;
     sum += grid[i].where((element) => element == 'O').length * multiplier;
+  }
+  for (List<String> line in grid) {
+    print(line.fold<String>("", (previousValue, element) => previousValue + element));
   }
   print(sum);
 }
