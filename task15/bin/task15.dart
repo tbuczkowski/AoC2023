8c576a09ad9be5014ee547c6b006094f1566983c
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-15 11:14:42 +0100

    task 15 part 2

--- task15/bin/task15.dart
+++ task15/bin/task15.dart
@@ -10,12 +10,44 @@ int computeHash(String word) {
   return currentValue;
 }
 
+final List<List<({String key, int focalLength})>> boxes = [];
+
 void main(List<String> arguments) async {
   final List<String> lines = loadInputData(arguments.first);
   final List<String> input = lines.first.split(',');
+  for (int i = 0; i < 256; i++) {
+    boxes.add([]);
+  }
+  for (String instruction in input) {
+    if (instruction.contains('-')) {
+      final String key = instruction.split('-').first;
+      final List<({String key, int focalLength})> box = boxes[computeHash(key)];
+      final int index = box.indexWhere((({String key, int focalLength}) element) => element.key == key);
+      if (index == -1) {
+        continue;
+      }
+      box.removeAt(index);
+      continue;
+    }
+    if (instruction.contains('=')) {
+      final [String key, String focalLengthStr] = instruction.split('=');
+      final List<({String key, int focalLength})> box = boxes[computeHash(key)];
+      final int index = box.indexWhere((({String key, int focalLength}) element) => element.key == key);
+      final lens = (key: key, focalLength: int.parse(focalLengthStr));
+      if (index == -1) {
+        box.add(lens);
+      } else {
+        box[index] = lens;
+      }
+    }
+  }
   int sum = 0;
-  for (String word in input) {
-    sum += computeHash(word);
+  for (int i = 0; i < 256; i++) {
+    final List<({String key, int focalLength})> box = boxes[i];
+    for (int j = 0; j < box.length; j++) {
+      final ({String key, int focalLength}) lens = box[j];
+      sum += (i + 1) * (j + 1) * lens.focalLength;
+    }
   }
   print(sum);
 }
