6ebce4a4a28d702b7f866f9a75e3d2b39da10946
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-16 09:16:09 +0100

    task16 part 2

--- task16/bin/task16.dart
+++ task16/bin/task16.dart
@@ -1,14 +1,30 @@
 import 'dart:io';
+import 'dart:math';
 
+import 'package:compute/compute.dart';
 import 'package:equatable/equatable.dart';
-
-final List<List<String>> grid = [];
-final Set<BeamNode> processedBeamNodes = {};
-final List<BeamNode> beamNodeProcessingQueue = [];
 
 typedef Coords = ({int x, int y});
 
-void tryAddBeamNode(BeamNode node) {
+int processBeam((List<List<String>> grid, BeamNode startNode) param) {
+  final Set<BeamNode> processedBeamNodes = {};
+  final List<BeamNode> beamNodeProcessingQueue = [];
+  beamNodeProcessingQueue.add(param.$2);
+  while (beamNodeProcessingQueue.isNotEmpty) {
+    final BeamNode? node =
+        beamNodeProcessingQueue.removeLast().evaluate(param.$1, processedBeamNodes, beamNodeProcessingQueue);
+    if (node != null) {
+      processedBeamNodes.add(node);
+    }
+  }
+  final Map<Coords, BeamNode> map = {};
+  for (BeamNode node in processedBeamNodes) {
+    map[node.coords] = node;
+  }
+  return map.length;
+}
+
+void tryAddBeamNode(Set<BeamNode> processedBeamNodes, List<BeamNode> beamNodeProcessingQueue, BeamNode node) {
   if (processedBeamNodes.contains(node)) {
     return;
   }
@@ -30,29 +46,34 @@ class BeamNode extends Equatable {
     return '{coords: $coords, source: $source, _direction: $_direction}';
   }
 
-  void evaluate() {
+  BeamNode? evaluate(
+      List<List<String>> grid, Set<BeamNode> processedBeamNodes, List<BeamNode> beamNodeProcessingQueue) {
     if (coords.x < 0 || coords.x >= grid.first.length || coords.y < 0 || coords.y >= grid.length) {
-      return;
+      return null;
     }
     final String symbol = grid[coords.y][coords.x];
     (switch ((symbol, _direction)) {
       ('.', _) || ('|', (x: 0, y: 1)) || ('|', (x: 0, y: -1)) || ('-', (x: 1, y: 0)) || ('-', (x: -1, y: 0)) => () {
-          tryAddBeamNode(BeamNode(coords: (x: coords.x + _direction.x, y: coords.y + _direction.y), source: coords));
+          tryAddBeamNode(processedBeamNodes, beamNodeProcessingQueue,
+              BeamNode(coords: (x: coords.x + _direction.x, y: coords.y + _direction.y), source: coords));
         },
       (r'\', (x: 1, y: 0)) || (r'\', (x: -1, y: 0)) || (r'/', (x: 0, y: 1)) || (r'/', (x: 0, y: -1)) => () {
-          tryAddBeamNode(BeamNode(coords: (x: coords.x - _direction.y, y: coords.y + _direction.x), source: coords));
+          tryAddBeamNode(processedBeamNodes, beamNodeProcessingQueue,
+              BeamNode(coords: (x: coords.x - _direction.y, y: coords.y + _direction.x), source: coords));
         },
       (r'\', (x: 0, y: 1)) || (r'\', (x: 0, y: -1)) || (r'/', (x: 1, y: 0)) || (r'/', (x: -1, y: 0)) => () {
-          tryAddBeamNode(BeamNode(coords: (x: coords.x + _direction.y, y: coords.y - _direction.x), source: coords));
+          tryAddBeamNode(processedBeamNodes, beamNodeProcessingQueue,
+              BeamNode(coords: (x: coords.x + _direction.y, y: coords.y - _direction.x), source: coords));
         },
       ('|', (x: 1, y: 0)) || ('|', (x: -1, y: 0)) || ('-', (x: 0, y: 1)) || ('-', (x: 0, y: -1)) => () {
-          tryAddBeamNode(BeamNode(coords: (x: coords.x + _direction.y, y: coords.y - _direction.x), source: coords));
-          tryAddBeamNode(BeamNode(coords: (x: coords.x - _direction.y, y: coords.y + _direction.x), source: coords));
+          tryAddBeamNode(processedBeamNodes, beamNodeProcessingQueue,
+              BeamNode(coords: (x: coords.x + _direction.y, y: coords.y - _direction.x), source: coords));
+          tryAddBeamNode(processedBeamNodes, beamNodeProcessingQueue,
+              BeamNode(coords: (x: coords.x - _direction.y, y: coords.y + _direction.x), source: coords));
         },
       _ => () {}
     })();
-    // print(beamNodeProcessingQueue);
-    processedBeamNodes.add(this);
+    return this;
   }
 
   @override
@@ -60,19 +81,22 @@ class BeamNode extends Equatable {
 }
 
 void main(List<String> arguments) async {
+  final List<List<String>> grid = [];
   final List<String> lines = loadInputData(arguments.first);
   for (String line in lines) {
     grid.add(line.split(''));
   }
-  beamNodeProcessingQueue.add(BeamNode(coords: (x: 0, y: 0), source: (x: -1, y: 0)));
-  while (beamNodeProcessingQueue.isNotEmpty) {
-    beamNodeProcessingQueue.removeLast().evaluate();
+  final List<Future<int>> futures = [];
+  for (int i = 0; i < grid.length; i++) {
+    futures.add(compute(processBeam, (grid, BeamNode(coords: (x: i, y: 0), source: (x: i, y: -1)))));
+    futures.add(
+        compute(processBeam, (grid, BeamNode(coords: (x: i, y: grid.length - 1), source: (x: i, y: grid.length)))));
+    futures.add(compute(processBeam, (grid, BeamNode(coords: (x: 0, y: i), source: (x: -1, y: i)))));
+    futures.add(
+        compute(processBeam, (grid, BeamNode(coords: (x: grid.length - 1, y: i), source: (x: grid.length, y: i)))));
   }
-  final Map<Coords, BeamNode> map = {};
-  for (BeamNode node in processedBeamNodes) {
-    map[node.coords] = node;
-  }
-  print(map.length);
+  final List<int> results = await Future.wait(futures);
+  print(results.reduce(max));
 }
 
 List<String> loadInputData(String filename) {
