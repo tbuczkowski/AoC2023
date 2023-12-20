d90a358ff01fe3e7631124e768356b925bc92df6
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-12-20 10:20:19 +0100

    task 20 part 1

---
+++ task20/bin/task20.dart
@@ -0,0 +1,120 @@
+import 'dart:collection';
+import 'dart:io';
+
+final Map<String, LogicNode> nodes = {};
+
+int highPulses = 0;
+int lowPulses = 0;
+
+final Queue<(String source, String destination, bool pulse)> pulsePropagationQueue = Queue();
+
+sealed class LogicNode {
+  final String key;
+  final List<String> outputs;
+  final Set<String> inputs = {};
+
+  LogicNode({
+    required this.key,
+    required this.outputs,
+  });
+
+  void registerInput(String input) {
+    inputs.add(input);
+  }
+
+  void emitPulse(bool pulse) {
+    for (String output in outputs) {
+      if (pulse) {
+        highPulses++;
+      } else {
+        lowPulses++;
+      }
+      pulsePropagationQueue.add((key, output, pulse));
+    }
+  }
+
+  void handlePulse(String source, bool pulse);
+}
+
+class Broadcaster extends LogicNode {
+  Broadcaster({
+    required super.key,
+    required super.outputs,
+  });
+
+  @override
+  void handlePulse(String source, bool pulse) {
+    emitPulse(pulse);
+  }
+}
+
+class FlipFlop extends LogicNode {
+  bool isOn = false;
+
+  FlipFlop({required super.key, required super.outputs});
+
+  @override
+  void handlePulse(String source, bool pulse) {
+    if (pulse) {
+      return;
+    }
+    isOn = !isOn;
+    emitPulse(isOn);
+  }
+}
+
+class Conjunction extends LogicNode {
+  Map<String, bool> memory = {};
+
+  Conjunction({required super.key, required super.outputs});
+
+  @override
+  void registerInput(String input) {
+    super.registerInput(input);
+    memory[input] = false;
+  }
+
+  @override
+  void handlePulse(String source, bool pulse) {
+    memory[source] = pulse;
+    final bool everyInputHasHighPulse = memory.values.every((bool rememberedPulse) => rememberedPulse);
+    emitPulse(!everyInputHasHighPulse);
+  }
+}
+
+void main(List<String> arguments) async {
+  final List<String> lines = loadInputData(arguments.first);
+  for (String line in lines) {
+    final [String nodeDefinition, String outputs] = line.split(' -> ');
+    final (String key, LogicNode node) = switch (nodeDefinition[0]) {
+      '%' => (nodeDefinition.substring(1), FlipFlop(key: nodeDefinition.substring(1), outputs: outputs.split(', '))),
+      '&' => (nodeDefinition.substring(1), Conjunction(key: nodeDefinition.substring(1), outputs: outputs.split(', '))),
+      _ => (nodeDefinition, Broadcaster(key: nodeDefinition, outputs: outputs.split(', '))),
+    };
+    nodes[key] = node;
+  }
+  for (LogicNode node in nodes.values) {
+    final List<String> outputs = node.outputs;
+    for (String output in outputs) {
+      nodes[output]?.registerInput(node.key);
+    }
+  }
+  for (int i = 0; i < 1000; i++) {
+    lowPulses++;
+    pulsePropagationQueue.add(('entrypoint', 'broadcaster', false));
+    while (pulsePropagationQueue.isNotEmpty) {
+      final (String source, String destination, bool pulse) = pulsePropagationQueue.removeFirst();
+      // print((source, destination, pulse));
+      nodes[destination]?.handlePulse(source, pulse);
+    }
+  }
+  print(lowPulses);
+  print(highPulses);
+  print(lowPulses * highPulses);
+}
+
+List<String> loadInputData(String filename) {
+  final File file = File(filename);
+  final List<String> lines = file.readAsLinesSync();
+  return lines;
+}
